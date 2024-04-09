797c16b563f06983a28ea285d5876a77ed1956d8
Oleg <oleg.tulinov128@gmail.com>
2024-04-09 17:45:49 +0700

    add layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrap">
+    <div class="shaft">
+      <div class="elevator">
+      </div>
+    </div>
+    <div class="buttons">
+      <button class="button" v-for=" floor  in  floors " :key="floor">
+        {{ floor }}
+      </button>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      floors: 5,
+    }
+  },
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.wrap {
+  display: flex;
+}
+
+.buttons {
+  display: flex;
+  flex-direction: column-reverse;
+  padding: 25px 10px;
+  gap: 75px
+}
+
+.button {
+  padding: 5px 10px;
+  border-radius: 50%;
+  border: 1px solid #111;
+}
+
+.elevator {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 90px;
+  height: 100px;
+  background-color: aqua;
+  transition: 1s;
+}
+
+.shaft {
+  display: flex;
+  align-items: end;
+  width: 90px;
+  height: 500px;
+  border: 1px solid #111;
 }
 </style>
